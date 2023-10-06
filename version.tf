64558bf57a4531f478863bcd4f724a5da620e662
Arindam Mitra <mail2arindam2003@yahoo.com>
2023-10-06 14:34:42 +0200

    Add files via upload

---
+++ version.tf
@@ -0,0 +1,15 @@
+##################
+## Versions:-
+##################
+
+terraform {
+
+  required_version = ">= 1.5.6"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.71.0"
+    }
+  }
+}
